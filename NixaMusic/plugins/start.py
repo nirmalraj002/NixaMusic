cf03a428b6611fab242593d1e4af3a4198986a39
❥︎ ɪᴛs-ɴɪxᴀ <122120240+NixaXD@users.noreply.github.com>
2023-02-03 17:06:47 +0600

    Update start.py

--- NixaMusic/plugins/start.py
+++ NixaMusic/plugins/start.py
@@ -20,7 +20,7 @@ async def start(event):
         return
     if event.is_private:
        await event.client.send_file(event.chat_id,
-             Config.START_IMG,
+             vars.START_IMG,
              caption=PM_START_TEXT.format(event.sender.first_name), 
              buttons=[
         [Button.url("➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ ➕", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
