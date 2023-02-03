051276f07b1052358b764747141e48e835270315
❥︎ ɪᴛs-ɴɪxᴀ <122120240+NixaXD@users.noreply.github.com>
2023-02-03 18:58:22 +0600

    Update __init__.py

--- NixaMusic/__init__.py
+++ NixaMusic/__init__.py
@@ -1,4 +1,4 @@
-  import os
+import os
 
 from telethon import TelegramClient
 from telethon import TelegramClient, events
@@ -13,7 +13,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 from vars import vars
-BOT_USERNAME = vars.BOT_USERNAME
+BOT_USERNAME = Config.BOT_USERNAME
 ASSISTANT_ID = []
 
 bot = TelegramClient('NixaMusic', api_id=vars.API_ID, api_hash=vars.API_HASH)
