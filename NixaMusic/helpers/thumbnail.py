5678743a94f407d68f8ee76ffd207014c9f70ef7
❥︎ ɪᴛs-ɴɪxᴀ <122120240+NixaXD@users.noreply.github.com>
2023-02-03 15:50:58 +0600

    Update thumbnail.py

--- NixaMusic/helpers/thumbnail.py
+++ NixaMusic/helpers/thumbnail.py
@@ -22,22 +22,22 @@ async def gen_thumb(thumbnail, title, userid, ctitle):
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
                 f = await aiofiles.open(
-                    f"resources/thumb{userid}.png", mode="wb"
+                    f"NixaMusic/resources/thumb{userid}.png", mode="wb"
                 )
                 await f.write(await resp.read())
                 await f.close()
     theme = random.choice(themes)
-    image1 = Image.open(f"resources/thumb{userid}.png")
-    image2 = Image.open(f"resources/{theme}.png")
+    image1 = Image.open(f"NixaMusic/resources/thumb{userid}.png")
+    image2 = Image.open(f"NixaMusic/resources/{theme}.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
     image6 = image4.convert("RGBA")
-    Image.alpha_composite(image5, image6).save(f"resources/temp{userid}.png")
-    img = Image.open(f"resources/temp{userid}.png")
+    Image.alpha_composite(image5, image6).save(f"NixaMusic/resources/temp{userid}.png")
+    img = Image.open(f"NixaMusic/resources/temp{userid}.png")
     draw = ImageDraw.Draw(img)
-    img.save(f"resources/final{userid}.png")
-    os.remove(f"resources/temp{userid}.png")
-    os.remove(f"resources/thumb{userid}.png")
-    final = f"resources/final{userid}.png"
+    img.save(f"NixaMusic/resources/final{userid}.png")
+    os.remove(f"NixaMusic/resources/temp{userid}.png")
+    os.remove(f"NixaMusic/resources/thumb{userid}.png")
+    final = f"NixaMusic/resources/final{userid}.png"
     return final
